44a839a950aa18cc79828f25987d7c621884fd07
Miguel Mendiola Aragones <mmendiol@c1r5s3.42madrid.com>
2024-05-02 12:39:42 +0200

    Adding the libft modified

---
+++ libft/includes/additional.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   additional.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmendiol <mmendiol@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/12 17:42:16 by mmendiol          #+#    #+#             */
+/*   Updated: 2024/04/22 17:45:24 by mmendiol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./libft.h"
+
+// FUNCTIONS ADDED LATER
+void	free_matrix(char **matrix);
+long	ft_atol(const char *str);
+int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strchr_bool(const char *s, int c);
