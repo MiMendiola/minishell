c1b9e6cf5e952e2e03be09c87f0559ce33a2a5dd
MiMendiola <mglmendiol@gmail.com>
2024-06-07 14:31:12 -0700

    Adding a type of expansor, I have to see the getenv is not working idk why

--- src/parsing/lexer.c
+++ src/parsing/lexer.c
@@ -6,11 +6,135 @@
 /*   By: lmntrix <lmntrix@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 18:12:11 by mmendiol          #+#    #+#             */
-/*   Updated: 2024/06/07 02:36:34 by lmntrix          ###   ########.fr       */
+/*   Updated: 2024/06/07 14:29:42 by lmntrix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+char *get_env_value(char *var)
+{
+    char *value;
+
+	value = getenv(var);
+    return (value ? value : "");
+}
+
+void copy_env_name(const char *src, char *dst)
+{
+	int i;
+
+	i = 0;
+    while (*src && *src != ' ' && *src != DQUOTES)
+		dst[i++] = *src++;
+    dst[i] = '\0';
+}
+
+void	expander(char *input)
+{
+	char	*src;
+	char	dst[256];
+	char	*env_value;
+
+	if (!input)
+		return ;
+	src = input;
+	while (*src)
+	{
+        if (*src == SQUOTES)
+		{
+			*input++ = *src++;
+			while (*src && *src != SQUOTES)
+				*input++ = *src++;
+			if (*src == SQUOTES)
+				*input++ = *src;
+		}
+		else if (*src == DQUOTES)
+		{
+			*input++ = *src++;
+			while (*src && *src != DQUOTES)
+			{
+				if (*input == '$')
+				{
+					if (ft_isdigit(src[1]))
+					{
+						src += 2;
+                        while (*src)
+                            *input++ = *src++;
+					}
+					else if (ft_isalpha(src[1]))
+					{
+						copy_env_name(src, dst);
+						src += ft_strlen(dst);
+						printf("%s\n", dst);
+						env_value = get_env_value(dst);
+						while (*env_value)
+                        	*input++ = *env_value++;
+					}
+				}
+				*input++ = *src++;
+			}
+			if (*src == DQUOTES)
+				*input++ = *src;
+		}
+		else
+            *input++ = *src;
+        src++;
+    }
+	*input = '\0';
+}
+
+void	remove_quotes(char *input)
+{
+	char	*src;
+	char	exterior_quote;
+
+	if (!input)
+		return ;
+	src = input;
+	exterior_quote = 0;
+	while (*src)
+	{
+        if ((*src == DQUOTES || *src == SQUOTES) && (exterior_quote == 0 || exterior_quote == *src))
+		{
+            if (exterior_quote == 0)
+                exterior_quote = *src;
+			else if (exterior_quote == *src)
+                exterior_quote = 0;
+        }
+		else
+            *input++ = *src;
+		src++;
+    }
+	*input = '\0';
+}
+
+void lexerize(t_token **tokens)
+{
+    int i;
+    t_token *aux;
+
+    if (!tokens || !*tokens)
+        return;
+    aux = *tokens;
+    while (aux) {
+        i = -1;
+        while (aux->tokens[++i])
+		{
+			expander(aux->tokens[i]);
+            remove_quotes(aux->tokens[i]);
+		}
+        aux = aux->next;
+    }
+}
+
+
+
+
+
+
+
+/*
 
 void	skip_quote(char *input)
 {
@@ -34,27 +158,6 @@ void	skip_quote(char *input)
 	}
 }
 
-void	remove_quotes(char *input, char quote)
-{
-	char	*dst;
-	char	*src;
-
-	if (!input)
-		return ;
-	dst = input;
-	src = input;
-	while (*src)
-	{
-		if (*src != quote)
-		{
-			*dst = *src;
-			dst++;
-		}
-		src++;
-	}
-	*dst = '\0';
-}
-
 int	quote_detector(const char *s)
 {
 	size_t	i;
@@ -62,9 +165,9 @@ int	quote_detector(const char *s)
 	i = 0;
 	while (s[i])
 	{
-		if (s[i] == '\'')
+		if (s[i] == SQUOTES)
 			return (1);
-		else if (s[i] == '\"')
+		else if (s[i] == DQUOTES)
 			return (2);
 		i++;
 	}
@@ -80,7 +183,7 @@ void	do_single_quotes(t_token *tokens)
 	{
 		if (quote_detector(tokens->tokens[i]) == 1)
 		{
-			remove_quotes(tokens->tokens[i], SQUOTES);
+			remove_quotes(tokens->tokens[i]);
 			printf("SINGLE %s\n", tokens->tokens[i]);
 		}
 		i++;
@@ -96,7 +199,7 @@ void	do_double_quotes(t_token *tokens)
 	{
 		if (quote_detector(tokens->tokens[i]) == 2)
 		{
-			remove_quotes(tokens->tokens[i], DQUOTES);
+			remove_quotes(tokens->tokens[i]);
 			printf("DOUBLE %s\n", tokens->tokens[i]);
 		}
 		i++;
@@ -108,12 +211,12 @@ void	lexerize(t_token **tokens)
 	int i;
 	t_token *aux;
 
-	i = -1;
 	aux = *tokens;
 	if (!tokens || !*tokens)
 		return ;
 	while (aux)
 	{
+		i = -1;
 		while (aux->tokens[++i])
 		{
 			if (quote_detector(aux->tokens[i]) == 1)
@@ -123,4 +226,4 @@ void	lexerize(t_token **tokens)
 		}
 		aux = aux->next;
 	}
-}
+}*/
